7d51aa9168189cc432c4d57730f5c0e7868a83bf
Seb Bacon <seb.bacon@gmail.com>
2023-12-31 17:29:05 +0000

    deal with inconsistent source zipping

--- grab.py
+++ grab.py
@@ -27,21 +27,25 @@ response.raise_for_status()
 
 # Step 2: Parse the HTML for the first ZIP file with the specified class
 soup = BeautifulSoup(response.text, 'html.parser')
-zip_link = None
+zip_link = csv_link = None
 
 for link in soup.find_all('a', class_='nhsd-a-box-link'):
     href = link.get('href', '')
     if href.endswith('.zip'):
         zip_link = href
         break
+    elif href.endswith('.csv'):
+        csv_link = href
+        break
 
-if not zip_link:
-    raise ValueError("No ZIP file found with the specified class.")
+if not zip_link and not csv_link:
+    raise ValueError("No zip or csv file found with the specified class.")
 
 # Step 3: Download the NHS ZIP file
-nhs_zip_url = urljoin(nhs_url, zip_link)
-nhs_zip_filename = nhs_zip_url.split('/')[-1]
-download_file(nhs_zip_url, nhs_zip_filename)
+link = zip_link or csv_link
+nhs_url = urljoin(nhs_url, link)
+nhs_filename = nhs_url.split('/')[-1]
+download_file(nhs_url, nhs_filename)
 
 # Step 4: Download the ePCN ZIP file
 epcn_url = "https://digital.nhs.uk/binaries/content/assets/website-assets/services/ods/data-downloads-other-nhs-organisations/epcn.zip"
@@ -60,7 +64,7 @@ with open(pcn_map_filename, 'w') as file:
 print("Downloaded pcn_map.json")
 
 # Step 6: Zip all the downloaded files into data.zip
-files_to_zip = [nhs_zip_filename, epcn_filename, pcn_map_filename]
+files_to_zip = [nhs_filename, epcn_filename, pcn_map_filename]
 zip_files(files_to_zip, "data.zip")
 
 # Optional: Clean up the original files after zipping if needed
